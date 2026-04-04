754c7c1e11668a96c0cd1b97c363649ccee7a893
sarwar-asik <sarwarasik@gmail.com>
2026-04-04 19:43:03 +0600

    feat(main): Add initial project boilerplate

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-medium">Portfolio</h1>
+    </main>
+  )
+}
+
+export default Home
